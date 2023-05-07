0facffa57ceaf33481faf351b9407f685d93530d
Trent Keyes <tkeyes@gmail.com>
2023-05-06 20:22:08 -0600

    Style front page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["Mulish", ...defaultTheme.fontFamily.sans],
       },
     },
   },
