52b18c0d237fb6122b58178c486f225b117f1f32
Trent Keyes <tkeyes@gmail.com>
2023-08-07 22:43:17 -0600

    Style hero, add stats and class descriptions cards, review carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,28 @@
-import ButtonCTA from "./ButtonCTA";
+import ButtonCTA from './ButtonCTA';
+// import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
-    <div className="pt-8 pb-8">
-      <div className="pb-8">
-        <h1 className="font-bold">Learning Spanish has never been so much fun!</h1>
-        <p className="py-6">Online lessons with a native Mexican teacher, in your time zone</p>
+    <div className="pt-8 pb-8 sm:flex sm:gap-4 border-green-500 sm:h-[calc(100vh-6rem)] sm:justify-center sm:items-center sm:py-0">
+      <div className="pb-8 sm:pb-0">
+        <h1 className="font-bold">
+          Learning Spanish has never been so much fun!
+        </h1>
+       
+        <h3 className='text-4xl my-4'>Hola, soy Anais</h3>
+        <p className="">
+          Online lessons with a native Mexican teacher, in your time zone
+        </p>
+        {/* <p className='py-4'>Learn how spanish speakers REALLY talk in fun, dynamic classes</p> */}
         <ButtonCTA>Take your first lesson free!</ButtonCTA>
+        
       </div>
 
-      <img className="rounded-xl" src="/src/assets/IMG_8898 conv.jpeg" alt="Anais smiling in a cafe" />
+      <img
+        className="rounded-xl sm:w-[45%]"
+        src="/src/assets/anaisSmile.jpeg"
+        alt="Anais smiling in a cafe"
+      />
     </div>
   );
 };
